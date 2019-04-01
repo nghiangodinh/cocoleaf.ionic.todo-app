cc3c2b7552ea9cf557cfcf6331fe73c6ce0c514a
Nghia <nghiangodinh@gmail.com>
2019-04-01 22:38:30 +0700

    first commit

---
+++ src/environments/environment.prod.ts
@@ -0,0 +1,11 @@
+export const environment = {
+  production: true,
+  firebase: {
+    apiKey: 'AIzaSyBha6MCu1Abp7YEJXykU2ZEOntXAqaPUnY',
+    authDomain: 'ionic4-c2405.firebaseapp.com',
+    databaseURL: 'https://ionic4-c2405.firebaseio.com',
+    projectId: 'ionic4-c2405',
+    storageBucket: 'ionic4-c2405.appspot.com',
+    messagingSenderId: '858913250780'
+  }
+};
